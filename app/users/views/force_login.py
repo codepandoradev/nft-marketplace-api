d6959e3b8e848b6204566217d89ce8b3fea64474
GentleEnvy <komarov.sergei163@gmail.com>
2022-10-29 20:07:46 +0300

    fix #4

--- app/users/views/force_login.py
+++ app/users/views/force_login.py
@@ -13,9 +13,9 @@ class UsersForceLoginView(BaseView):
     serializer_class = schema_serializer('ForceLogin', token=CharField())
 
     def get(self):
-    if not settings.DEBUG:
-        raise CriticalError("force_login is unavailable when not DEBUG")
-    auth_service = AuthService()
-    user = User.objects.get(id=1)
-    token = auth_service.login(user, self.request).key
-    return Response({'token': token})
+        if not settings.DEBUG:
+            raise CriticalError("force_login is unavailable when not DEBUG")
+        auth_service = AuthService()
+        user = User.objects.get(id=1)
+        token = auth_service.login(user, self.request).key
+        return Response({'token': token})
