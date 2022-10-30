5da0308d60647cbd86384bbe155dddf0ebc4de15
GentleEnvy <komarov.sergei163@gmail.com>
2022-10-30 13:22:50 +0300

    fix #38

--- app/base/serializers/base.py
+++ app/base/serializers/base.py
@@ -22,7 +22,7 @@ class BaseSerializer(serializers.Serializer):
         }
 
     def is_valid(self, raise_exception=True):
-        return super().is_valid(raise_exception)
+        return super().is_valid(raise_exception=raise_exception)
 
     def update(self, instance, validated_data):
         pass
@@ -35,7 +35,7 @@ class BaseModelSerializer(serializers.ModelSerializer, BaseSerializer):
     Meta: type
 
     def is_valid(self, raise_exception=True):
-        return super().is_valid(raise_exception)
+        return super().is_valid(raise_exception=raise_exception)
 
     def get_extra_kwargs(self):
         extra_kwargs = super().get_extra_kwargs()
