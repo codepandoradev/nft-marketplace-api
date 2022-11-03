b2b2a2fd86d344c9182c9dc234202d7ac5df1d1f
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-11-03 14:25:38 +0300

    add TestUserNotExist(working)

--- app/users/tests/views/test_general.py
+++ app/users/tests/views/test_general.py
@@ -1,4 +1,6 @@
+from app.base.tests.fakers import fake
 from app.base.tests.views.base import BaseViewTest
+from app.users.serializers.token import POST_UsersTokenSerializer
 
 
 class UsersTest(BaseViewTest):
@@ -13,6 +15,17 @@ class UsersTest(BaseViewTest):
             {
                 'username': user.username,
                 'password': user.raw_password,
+            },
+            format='multipart',
+        )
+
+    def test_user_not_exist(self):
+        self._test(
+            'post',
+            POST_UsersTokenSerializer.WARNINGS[401],
+            {
+                'username': fake.english_word(),
+                'password': fake.english_word(),
             },
             format='multipart',
         )
