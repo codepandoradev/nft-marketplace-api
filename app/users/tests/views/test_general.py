133926b9a8155944608fbe42c674ebdf1f3c512e
Zodefall <104734145+Zodefall@users.noreply.github.com>
2022-11-04 13:26:28 +0300

    add all tests

--- app/users/tests/views/test_general.py
+++ app/users/tests/views/test_general.py
@@ -1,10 +1,9 @@
-
 from app.base.tests.fakers import fake
 from app.base.tests.views.base import BaseViewTest
 from app.users.serializers.token import POST_UsersTokenSerializer
-
 from app.base.tests.views.base import BaseViewTest
-
+from app.users.enums.users import UserType
+from app.users.models import User, Token
 
 
 class UsersTest(BaseViewTest):
@@ -20,7 +19,7 @@ class UsersTest(BaseViewTest):
                 'username': user.username,
                 'password': user.raw_password,
             },
-            format='multipart',
+            format='json',
         )
 
     def test_user_not_exist(self):
@@ -30,7 +29,31 @@ class UsersTest(BaseViewTest):
             {
                 'username': fake.english_word(),
                 'password': fake.english_word(),
-
             },
-            format='multipart',
+            format='json',
         )
+
+    def test_user_banned(self):
+        user = self.me
+        self.client.logout()
+        user.type = UserType.BANNED
+        user.save()
+        self._test(
+            'post',
+            POST_UsersTokenSerializer.WARNINGS[401],
+            {
+                'username': user.username,
+                'password': user.raw_password,
+            },
+            format='json',
+        )
+
+    def test_delete_user(self):
+        user = self.me
+        self._test(
+            'delete',
+            {},
+            {},
+            format='json',
+        )
+        self.assert_equal(Token.objects.count(), 0)
