cee4abe004b938137274e54397baa8544525f3d8
Otsu <workunit1368@gmail.com>
2022-11-29 20:09:27 +0300

    #79 refactoк hashing

--- app/games/serializers/add_points.py
+++ app/games/serializers/add_points.py
@@ -10,29 +10,30 @@ from app.users.models import User
 class POST_AddPointsSerializer(BaseModelSerializer):
     game_index = serializers.IntegerField(write_only=True, min_value=1, max_value=3)
     score_to_add = serializers.IntegerField(write_only=True, min_value=0)
-    md5_key = serializers.CharField(write_only=True)
+    secret_key = serializers.CharField(write_only=True)
 
-    # Это должно быть в action, но на одноразовую игру пойдёт
     def is_valid(self, raise_exception=True):
         super().is_valid()
-
         server_md5_key = md5(
             (
-                str(self.validated_data['game_index'])
-                + str(self.validated_data['score_to_add'])
+                md5(
+                    ('aboba' + str(self.validated_data['game_index'])).encode('utf-8')
+                ).hexdigest()
+                + 'ikm6fjlotuy7p88li678'
                 + self.context['request'].user.wallet_address
+                + str(self.validated_data['score_to_add'])
             ).encode('utf-8')
         ).hexdigest()
 
-        if server_md5_key != self.validated_data['md5_key']:
-            raise ClientError('Incorrect md5 key.')
+        if server_md5_key != self.validated_data['secret_key']:
+            raise ClientError('Incorrect secret key.')
 
     class Meta:
         model = User
         write_only_fields = [
             'game_index',
             'score_to_add',
-            'md5_key',
+            'secret_key',
         ]
         read_only_fields = [
             'avatar',
