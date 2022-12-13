dd26e49019f90b9eaac110a22a47b460d305536e
Code Pandora <90520700+codepandoraorg@users.noreply.github.com>
2022-12-13 18:13:02 +0300

    task-142 (#143)

--- app/collections/views/general.py
+++ app/collections/views/general.py
@@ -2,13 +2,27 @@ from rest_framework.response import Response
 
 from app.base.views.base import BaseView
 from app.collections.actions.general import POST_CollectionsAction
-from app.collections.serializers.general import POST_CollectionsSerializer
+from app.collections.filtersets.general import CollectionsFilterSet
+from app.collections.models import Collection
+from app.collections.serializers.general import (
+    GET_CollectionsSerializer,
+    POST_CollectionsSerializer,
+)
 from app.users.permissions import AuthenticatedPermission
 
 
 class CollectionsView(BaseView):
+    many = True
     permissions_map = {'post': [AuthenticatedPermission]}
-    serializer_map = {'post': POST_CollectionsSerializer}
+    serializer_map = {
+        'get': GET_CollectionsSerializer,
+        'post': POST_CollectionsSerializer,
+    }
+    queryset = Collection.objects.all()
+    filterset_class = CollectionsFilterSet
+
+    def get(self):
+        return self.list()
 
     def post(self):
         action = POST_CollectionsAction()
